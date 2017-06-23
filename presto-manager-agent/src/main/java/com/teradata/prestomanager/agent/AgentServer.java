ccf31e5347ff69fb016ba945fa469efe408c3133
Jacob Komissar <Jacob.Komissar@teradata.com>
2017-06-23 14:47:14 -0400

    Modify AgentServer and ServerBuilder after code reviews
    Add license headers and TODOs; Remove unnecessary comments

--- presto-manager-agent/src/main/java/com/teradata/prestomanager/agent/AgentServer.java
+++ presto-manager-agent/src/main/java/com/teradata/prestomanager/agent/AgentServer.java
@@ -1,37 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.teradata.prestomanager.agent;
 
 import com.teradata.prestomanager.common.ServerBuilder;
 import org.eclipse.jetty.server.Server;
 
-import java.net.BindException;
-
 class AgentServer
 {
+    // TODO: Replace placeholder constants with configurable properties
     private static final int PORT = 8081;
     private static final String URI = "http://localhost/";
 
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
+            throws Exception
     {
-        ServerBuilder sb = new ServerBuilder();
-        sb.setPort(8081);
-        sb.setURI("http://localhost/");
-        // sb.registerComponent(Resource.class);
-        Server server = sb.build();
+        Server server = new ServerBuilder()
+                .setURI(URI)
+                .setPort(PORT)
+                .build();
 
         try {
             server.start();
-        }
-        catch (BindException e) {
-            e.printStackTrace();
-            System.out.println("BindException: " + e.getMessage());
-            return;
-        }
-        try {
             server.join();
         }
+        catch (Exception e) {
+            // TODO: Log exception
+            throw e;
+        }
         finally {
-            System.out.println("Stopping");
-            server.stop();
+            try {
+                server.stop();
+            }
+            catch (Exception e) {
+                // TODO: Log exception
+                throw e;
+            }
         }
     }
 }
