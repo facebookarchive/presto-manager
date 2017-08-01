62c23bcf3e4b2a7f8ef537dabc82469c3eb0a6b7
Shan Bai <shan.bai@teradata.com>
2017-08-01 15:49:17 -0400

    Fix DateParameter and corresponding log API

--- presto-manager-agent/src/main/java/com/teradata/prestomanager/agent/api/LogsAPI.java
+++ presto-manager-agent/src/main/java/com/teradata/prestomanager/agent/api/LogsAPI.java
@@ -14,7 +14,6 @@
 package com.teradata.prestomanager.agent.api;
 
 import com.teradata.prestomanager.agent.LogsHandler;
-import com.teradata.prestomanager.common.JaxrsParameter;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -32,11 +31,6 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import java.time.Instant;
-import java.time.chrono.IsoChronology;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoField;
 
 @Path("/logs")
 @Api(description = "the logs API")
@@ -45,20 +39,6 @@ import java.time.temporal.ChronoField;
         date = "2017-06-23T09:53:13.549-04:00")
 public class LogsAPI
 {
-    private static final String DEFAULT_DATE = "DEFAULT";
-
-    private static final DateTimeFormatter DATE_FORMAT = new DateTimeFormatterBuilder()
-            .parseCaseInsensitive().parseStrict()
-            .appendPattern("yyyy-MM-dd['T'HH:mm[:ss[")
-            .appendFraction(ChronoField.NANO_OF_SECOND, 0, 3, true)
-            .appendPattern("][XX][XXX]]]")
-            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-            .parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
-            .parseDefaulting(ChronoField.OFFSET_SECONDS, 0)
-            .toFormatter().withChronology(IsoChronology.INSTANCE);
-
     @GET
     @Produces({MediaType.TEXT_PLAIN})
     @ApiOperation(value = "Get a listing of log files")
@@ -78,8 +58,8 @@ public class LogsAPI
             @ApiResponse(code = 404, message = "Resource not found")})
     public Response getLog(
             @PathParam("file") @ApiParam("The name of a file") String file,
-            @QueryParam("from") @ApiParam("Ignore logs before this date") @DefaultValue(DEFAULT_DATE) DateParameter fromDate,
-            @QueryParam("to") @ApiParam("Ignore logs after this date") @DefaultValue(DEFAULT_DATE) DateParameter toDate,
+            @QueryParam("from") @ApiParam("Ignore logs before this date") Instant fromDate,
+            @QueryParam("to") @ApiParam("Ignore logs after this date") Instant toDate,
             @QueryParam("level") @ApiParam("Only get logs of this level") @DefaultValue(LogsHandler.DEFAULT_LOG_LEVEL) String level,
             @QueryParam("n") @ApiParam("The maximum number of log entries to get") Integer maxEntries)
     {
@@ -96,31 +76,8 @@ public class LogsAPI
             @ApiResponse(code = 404, message = "Resource not found")})
     public Response deleteLog(
             @PathParam("file") @ApiParam("The name of a file") String file,
-            @QueryParam("to") @ApiParam("Ignore logs after this date") @DefaultValue(DEFAULT_DATE) DateParameter toDate)
+            @QueryParam("to") @ApiParam("Ignore logs after this date") Instant toDate)
     {
         return LogsHandler.deleteLogs(file, toDate);
-    }
-
-    public static class DateParameter
-            extends JaxrsParameter<Instant>
-    {
-        public DateParameter(String s)
-        {
-            super(s);
-        }
-
-        @Override
-        protected Instant parseString(String s)
-                throws ParseException
-        {
-            try {
-                return (s == null || DEFAULT_DATE.equalsIgnoreCase(s))
-                        ? null
-                        : DATE_FORMAT.parse(s, Instant::from);
-            }
-            catch (DateTimeParseException e) {
-                throw new ParseException();
-            }
-        }
     }
 }
